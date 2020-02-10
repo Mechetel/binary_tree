dbf9fc027cd17008a7fe3db5cec924daf34b0190
mechetel <dima.homa5@gmail.com>
2020-02-10 18:15:25 +0200

    initial commit for binary tree, apriorit work

---
+++ apriorit/ITraversalStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace apriorit
+{
+    public interface ITraversalStrategy<T> where T : IComparable<T>
+    {
+        IEnumerator<T> Traversal(BinaryTreeNode<T> node);
+    }
+}
